176a96852cf43a4d182b601ae5456662dc57e411
robinafro <oble.ro.2023@skola.ssps.cz>
2024-08-24 11:16:10 +0100

    Scheduled reminders

--- src/modules/api.py
+++ src/modules/api.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request
 from . import db
 from .db import Model, StringField, NumberField
-import time
+import time, threading
 
 # DAY_DURATION = 60 * 60 * 24
 DAY_DURATION = 5
+REMINDER_UPDATE_INTERVAL = 1
 
 class Habit(Model):
     class Meta:
@@ -15,6 +16,7 @@ class Habit(Model):
             'duration': NumberField(default=30),
             'time': NumberField(default=0), # Seconds since midnight
             'last_completed': NumberField(default=0),
+            'last_reminded': NumberField(default=0),
             'streak': NumberField(default=0),
         }
 
@@ -105,3 +107,25 @@ def habits_complete():
             return {'success': False, 'error': 'Habit not found'}, 404
     except Exception as e:
         return {'success': False, 'error': str(e)}, 400
+
+def reminder():
+    while True:
+        habits = db.read('habits', default=[])
+        for habit in habits:
+            habit_obj = Habit(**habit)
+            # is it time to remind?
+            # habit_obj.frequency * DAY_DURATION must have passed since last reminder
+            # and the current time must be within the time range
+            if time.time() - habit_obj.last_reminded >= habit_obj.frequency * DAY_DURATION and habit_obj.time <= time.time() % (DAY_DURATION * habit_obj.frequency) < habit_obj.time + REMINDER_UPDATE_INTERVAL:
+                print(f'Reminder: {habit_obj.name}')
+                habit_obj.last_reminded = time.time()
+                habits = [h for h in habits if h.get('id') != habit_obj.id]
+                habits.append(habit_obj.serialize())
+                db.save('habits', habits)
+
+        time.sleep(REMINDER_UPDATE_INTERVAL)
+
+def start_reminder():
+    threading.Thread(target=reminder, daemon=True).start()
+
+start_reminder()
